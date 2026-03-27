d94e2dab8967eacc78ee322aa31ebf6eaa7fd5d5
amc19008 <143828530+amc19008@users.noreply.github.com>
2026-03-27 01:44:33 -0400

    Update BLASTII_py_for_all.sh

--- Scripts/BLASTII_py_for_all.sh
+++ Scripts/BLASTII_py_for_all.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=final_py_all
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH -c 1
-#SBATCH --mem=35G
-#SBATCH --qos=general
-#SBATCH --partition=general
-#SBATCH --mail-user=asher.coello@uconn.edu
-#SBATCH --mail-type=END
-#SBATCH -o %x_%a.out
-#SBATCH -e %x_%a.err  
-
-module load biopython
 
 while read line
         do
-        file="/core/globus/cgi/RAMP/gene_fam/BLAST/$line-blastx.Scorpiones_db_Column1.txt" #This is now the list of files
-        Fasta="/core/globus/cgi/RAMP/gene_fam/BLAST/$line.fasta"
+        file="/core/globus/cgi/RAMP/gene_fam/BLAST/${line}-blastx.Scorpiones_db_Column1.txt" #This is now the list of files
+        Fasta="/core/globus/cgi/RAMP/gene_fam/BLAST/${line}.fasta"
 #Step 2: Make a list of battle types to be analyzed
 #Reads the input file one line at a time
 #pulls colony type (e.g., AB), by cutting (cut) at commas (-d ,) and keeping the first field (-f1)
@@ -24,14 +11,14 @@ while read line
 #directs output to a new file: temp3-$colonies-$castes.txt (note that this will overwrite any previous file by that name)
 while read line
         do
-                SeqID=$(echo "$line" | cut -d , -f1)
+                SeqID=$(echo "$line" | cut -d . -f1)
                 geneName="temp"
         python getSeq.py $Fasta $SeqID $geneName  
 
         wait
         
      done <$file
-cat *.temp.fasta >> Toxin_output.fasta
+cat *.temp.fasta >> Multi_Toxin_output.fasta
 rm *.temp.fasta
 
 done <new_list.txt
